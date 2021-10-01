9e7b53ebc0a24558315694c95bdaaa4c039cf4b1
db2 <db2@z4r4k1.corp.popular.local>
2021-09-30 21:07:14 -0500

    add authentication and create crud of list products

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.db import router
+from rest_framework.routers import DefaultRouter
 from django.urls import path
+from product.views import ProductViewset
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+router = DefaultRouter()
+router.register('product', ProductViewset)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('api/token',TokenObtainPairView.as_view(),name='token_obtain_pair'),
+    path('api/token/refresh',TokenRefreshView.as_view(),name='token_refresh'),
+]+ router.urls
--- product/views.py
+++ product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from product.serializers import ProductSerializer
+from product.models import Product
 # Create your views here.
+class ProductViewset(ModelViewSet):
+    queryset=Product.objects.all()
+    serializer_class = ProductSerializer
